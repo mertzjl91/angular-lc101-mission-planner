89572a0afd24f729da989ccc8eff31333daa17ef
Jessica Mertz <mertzjl91@gmail.com>
2021-07-02 08:31:46 -0500

    Studio completed with one bonus mission.

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
-
+  memberBeingEdited: object = null;
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
@@ -18,4 +18,34 @@ export class CrewComponent implements OnInit {
   ngOnInit() {
   }
 
+  add(memberName: string, isFirst: boolean) {
+    if(this.memberExists(memberName) === false) { 
+      this.crew.push({name: memberName, firstMission: isFirst});
+    }
+  }
+  
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+  
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+  }
+  
+  memberExists(newName: string) {
+    for(let i = 0; i < this.crew.length; i++) {
+       if(this.crew[i]['name'] === newName) {
+         return true;
+       }
+    console.log(this.crew[i]);
+    }
+    return false;
+  }
 }
+
